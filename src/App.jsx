68d18f63894208464f6fffffa99aebb3546b743b
jacques28 <devjacques28i@gmail.com>
2022-12-23 19:22:14 -0500

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <div className="App"> 
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Suscipit ipsum placeat ab laboriosam et tempora corporis quaerat commodi eveniet a, inventore sit reprehenderit laborum unde culpa alias error omnis at?</p> 
+    <div>
+      <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi tenetur quidem voluptates ut blanditiis vitae illum animi minima vel pariatur dolorem, a odio debitis fugit ex reprehenderit illo ad repellendus?</span>
+    </div>     
+    </div>
+  )
+}
+
+export default App
